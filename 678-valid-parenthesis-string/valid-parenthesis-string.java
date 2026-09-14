dd64e08dc5c774aa8890df6fb0656af7db744547
Akshat Kotecha <akshatkotecha@gmail.com>
2026-09-14 14:00:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (92.27%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int open=0;
+        int close=0;
+        int n=s.length()-1;
+        for(int i=0;i<=n;i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='*') open++;
+            else open--;
+            if(s.charAt(n-i)==')' || s.charAt(n-i)=='*') close++;
+            else close--;
+            if(open<0 || close<0) return false;
+        }
+        return true;
+    }
+}
